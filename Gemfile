d313b2395b4bc0d9c4c1579283273564971dceed
Gerald Kalafut <gerald@kalafut.org>
2011-11-29 18:58:37 -0700

    2011.11.29.1 github / rubygem push

--- Gemfile
+++ Gemfile
@@ -1,5 +1,2 @@
 source "http://rubygems.org"
 gemspec
-
-gem 'ghaki-app',    :git => 'ghaki@ghaki.com:/home/ghaki/git-archive/ghaki-app.git'
-gem 'ghaki-logger', :git => 'ghaki@ghaki.com:/home/ghaki/git-archive/ghaki-logger.git'
--- setup.bash
+++ setup.bash
@@ -1,3 +1,8 @@
+export GK_PROJECT_IDEPS=( \
+  "${GK_PROJECT_DIR}/../ghaki-app/lib" \
+  "${GK_PROJECT_DIR}/../ghaki-logger/lib" \
+  )
+
 export GK_PROJECT_GO_DIRS=( \
   "lib:${GK_PROJECT_DIR}/lib/ghaki/stats" \
   "spec:${GK_PROJECT_DIR}/spec/ghaki/stats" \
